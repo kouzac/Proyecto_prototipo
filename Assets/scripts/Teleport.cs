dfe7ead454d015c591a3aa54fd6c707870de9322
kouzac <99357782+kouzac@users.noreply.github.com>
2022-10-03 21:25:32 -0500

    portal

--- Assets/scripts/Teleport.cs
+++ Assets/scripts/Teleport.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
 
 public class Teleport : MonoBehaviour
 {
-    
+    public float speed;
+    public string scene;
     void Start()
     {
         
@@ -12,6 +15,18 @@ public class Teleport : MonoBehaviour
     
     void Update()
     {
-        
+          transform.Translate(Vector2.left * speed * Time.deltaTime);
+        if (transform.position.x <= -11.9)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(scene);
+            Destroy(gameObject);
+        }
     }
 }
